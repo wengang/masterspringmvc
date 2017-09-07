aa2554811888e588586c5bf1752708a9565562fc
yuwengang <tao-yu@hotmail.com>
2017-09-07 15:00:58 +0800

    handle file upload error

--- src/main/java/io/metaphor/masterSpringMvc/config/WebConfiguration.java
+++ src/main/java/io/metaphor/masterSpringMvc/config/WebConfiguration.java
@@ -1,11 +1,15 @@
 package io.metaphor.masterSpringMvc.config;
 
 import io.metaphor.masterSpringMvc.date.CNLocalDateFormatter;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.web.servlet.ErrorPage;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.multipart.MultipartException;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -37,5 +41,10 @@ public class WebConfiguration extends WebMvcConfigurerAdapter {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Bean
+    public EmbeddedServletContainerCustomizer containerCustomizer() {
+        return container ->  container.addErrorPages(new ErrorPage(MultipartException.class,"/uploadError"));
     }
 }
--- src/main/java/io/metaphor/masterSpringMvc/profile/PictureUploadController.java
+++ src/main/java/io/metaphor/masterSpringMvc/profile/PictureUploadController.java
@@ -7,13 +7,13 @@ import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.util.WebUtils;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.net.URLConnection;
@@ -45,14 +45,15 @@ public class PictureUploadController {
     @RequestMapping(value = "upload",method = RequestMethod.POST)
     public String onUpload(MultipartFile file, RedirectAttributes redirectAttributes,
                            Model model) throws IOException {
-        if(file.isEmpty() || !isImage(file)) {
-            redirectAttributes.addFlashAttribute("error","Incorrect file. " +
-                    "Please upload a picture.");
-            return "redirect:/upload";
-        }
-        Resource picturePath = copyFileToPictrues(file);
-        model.addAttribute("picturePath",picturePath);
-        return "profile/uploadPage";
+        throw new IOException("Some message");
+//        if(file.isEmpty() || !isImage(file)) {
+//            redirectAttributes.addFlashAttribute("error","Incorrect file. " +
+//                    "Please upload a picture.");
+//            return "redirect:/upload";
+//        }
+//        Resource picturePath = copyFileToPictrues(file);
+//        model.addAttribute("picturePath",picturePath);
+//        return "profile/uploadPage";
     }
     @RequestMapping(value = "/uploadedPicture")
     public void getUploadedPicture(HttpServletResponse response,
@@ -61,7 +62,19 @@ public class PictureUploadController {
         response.setHeader("Content-Type", URLConnection.guessContentTypeFromName(picturePath.toString()));
         IOUtils.copy(picturePath.getInputStream(),response.getOutputStream());
     }
+    @ExceptionHandler(IOException.class)
+    public ModelAndView handleIOException(IOException exception) {
+        ModelAndView modelAndView = new ModelAndView("profile/uploadPage");
+        modelAndView.addObject("error",exception.getMessage());
+        return modelAndView;
+    }
 
+    @RequestMapping("uploadError")
+    public ModelAndView onUploadError(HttpServletRequest request) {
+        ModelAndView modelAndView = new ModelAndView("profile/uploadPage");
+        modelAndView.addObject("error",request.getAttribute(WebUtils.ERROR_MESSAGE_ATTRIBUTE));
+        return modelAndView;
+    }
     private boolean isImage(MultipartFile file) {
         return file.getContentType().startsWith("image");
     }
