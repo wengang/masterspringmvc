bc031845ea5ff7225986a4ba2bde1cd1a6aafa9b
yuwengang <tao-yu@hotmail.com>
2017-09-08 19:12:08 +0800

    secret app,sec tag

--- src/main/java/io/metaphor/masterSpringMvc/user/api/UserApiController.java
+++ src/main/java/io/metaphor/masterSpringMvc/user/api/UserApiController.java
@@ -6,12 +6,14 @@ import io.metaphor.masterSpringMvc.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@Secured("ROLE_ADMIN")
 public class UserApiController {
     private UserRepository userRepository;
 
