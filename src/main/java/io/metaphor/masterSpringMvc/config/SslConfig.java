61ed95841d7095735f9c5213b8c96306d7401ae1
wengang <tao-yu@hotmail.com>
2017-09-10 22:24:10 +0800

    redis session and ssl configuration

---
+++ src/main/java/io/metaphor/masterSpringMvc/config/SslConfig.java
@@ -0,0 +1,36 @@
+package io.metaphor.masterSpringMvc.config;
+
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http11.Http11NioProtocol;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+
+@Configuration
+public class SslConfig {
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() throws IOException {
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+        tomcat.addAdditionalTomcatConnectors(createSslConnector());
+        return tomcat;
+    }
+
+    private Connector createSslConnector() throws IOException {
+        Connector connector = new Connector(Http11NioProtocol.class.getName());
+        Http11NioProtocol protocol = (Http11NioProtocol)connector.getProtocolHandler();
+        connector.setPort(8443);
+        connector.setSecure(true);
+        connector.setScheme("https");
+        protocol.setSSLEnabled(true);
+        protocol.setKeyAlias("masterspringmvc");
+        protocol.setKeystorePass("password");
+        protocol.setKeyPass("password");
+        protocol.setKeystoreFile(new ClassPathResource("tomcat.keystore").getFile().getAbsolutePath());
+        protocol.setSslProtocol("TLS");
+        return connector;
+    }
+}
--- src/main/java/io/metaphor/masterSpringMvc/config/WebSecurityConfiguration.java
+++ src/main/java/io/metaphor/masterSpringMvc/config/WebSecurityConfiguration.java
@@ -10,7 +10,9 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.formLogin()
+        http.requiresChannel().anyRequest().requiresSecure()
+                .and()
+                .formLogin()
                 .loginPage("/login")
                 .defaultSuccessUrl("/profile")
                 .and()
