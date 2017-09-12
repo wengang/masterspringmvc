00e4ac2969b38964398a790084c680d08ffef2e9
yuwengang <tao-yu@hotmail.com>
2017-09-12 19:55:25 +0800

    Etag

--- src/main/java/io/metaphor/masterSpringMvc/MasterSpringMvcApplication.java
+++ src/main/java/io/metaphor/masterSpringMvc/MasterSpringMvcApplication.java
@@ -4,12 +4,20 @@ import io.metaphor.masterSpringMvc.config.PictureUploadProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import javax.servlet.Filter;
+
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
+
 
 @SpringBootApplication
 @EnableConfigurationProperties({PictureUploadProperties.class})
 public class MasterSpringMvcApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MasterSpringMvcApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MasterSpringMvcApplication.class, args);
+    }
+
+
 }
---
+++ src/main/java/io/metaphor/masterSpringMvc/config/WebConfig.java
@@ -0,0 +1,27 @@
+package io.metaphor.masterSpringMvc.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
+
+import javax.servlet.Filter;
+
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public Filter etagFilter() {
+        return new ShallowEtagHeaderFilter();
+    }
+    @Bean
+    public FilterRegistrationBean etagFilterRegistration() {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(etagFilter());
+        registration.addUrlPatterns("/*");
+//        registration.addInitParameter("paramName", "paramValue");
+        registration.setName("EtagFilter");
+//        registration.setOrder(6);
+        return registration;
+    }
+}
